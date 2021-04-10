33df5392e07dd2ef963d0d0562341873070606f2
Raphael Nussbaumer <e1526647@student.tuwien.ac.at>
2021-04-10 15:20:55 +0200

    make db results picklable for parallel processing

--- src/db.py
+++ src/db.py
@@ -1,24 +1,30 @@
 import psycopg2
 import psycopg2.extras
 from util import drop0x
-from common import IdCodePairs
+from common import IdCodePairs, IdCodePair
+from typing import Tuple
 
 conn = psycopg2.connect('dbname=e user=rapha password=lilpeep@Hohenems')
 
 def getCodesViaIds(codeIds) -> IdCodePairs:
-  cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+  cur = conn.cursor()
   cur.execute('select id, dat as code from bindata where id = ANY(%s);', (codeIds,))
-  return cur.fetchall()
+  return [IdCodePair(r[0], bytes(r[1])) for r in cur.fetchall()]
 
 def getCodesViaAccountIds(aid) -> IdCodePairs:
-  cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+  cur = conn.cursor()
   cur.execute('select b.id, dat as code from bindata b, contract2 c where c.aid = any(%s) and c.cdeployed = b.id' , (aid,))
-  return cur.fetchall()
+  return [IdCodePair(r[0], bytes(r[1])) for r in cur.fetchall()]
 
 def getCodesViaAddresses(address) -> IdCodePairs:
-  cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+  cur = conn.cursor()
   address = [bytes.fromhex(drop0x(a)) for a in address]
   cur.execute("""select b.id, dat as code from bindata b, contract2 c, account a
     where a.addr = any(%s) and c.cdeployed = b.id and a.id = c.aid
     """ , (address,))
-  return cur.fetchall()
+  return [IdCodePair(r[0], bytes(r[1])) for r in cur.fetchall()]
+
+def selectIdCodePairs(query: str, vars: Tuple = None) -> IdCodePairs:
+  cur = conn.cursor()
+  cur.execute(query, vars)
+  return [IdCodePair(r[0], bytes(r[1])) for r in cur.fetchall()]
