5c83e728aa6a0ccc0bf68c8eeb88f54b064c2495
Raphael Nussbaumer <e1526647@student.tuwien.ac.at>
2022-03-28 17:57:01 +0200

    bzHash: map to numbers and letters

--- src/hashes/bz.py
+++ src/hashes/bz.py
@@ -4,6 +4,8 @@ from util import discretize, sdFromMean
 def bzCompRatio(bs: bytes):
   return len(bz2.compress(bs)) / len(bs) if len(bs) > 0 else 1
 
+_map = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_' # 64 symbols
+
 def hashFixedLen(bs:bytes, chunkLen=200, chunkRes=8) -> str:
   l = len(bs)
   if l == 0:
@@ -13,10 +15,10 @@ def hashFixedLen(bs:bytes, chunkLen=200, chunkRes=8) -> str:
   bs += b'\x00' * pad
   res = [bzCompRatio(bs[i:i+chunkLen]) for i in range(0, l, chunkLen)]
   res = sdFromMean(res)
-  return ''.join(chr(0xb0 + discretize(-2, 2, chunkRes, val)) for val in res)
+  return ''.join(_map[discretize(-2, 2, chunkRes, val)] for val in res)
 
 def hashJumpi(bs:bytes, chunkRes=8) -> str:
   jumpi = b'\x57'
   res = [bzCompRatio(chunk) for chunk in bs.split(jumpi)]
   res = sdFromMean(res)
-  return ''.join(chr(0xb0 + discretize(-2, 2, chunkRes, val)) for val in res)
+  return ''.join(_map[discretize(-2, 2, chunkRes, val)] for val in res)
